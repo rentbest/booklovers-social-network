3c0eb8ad8d0f92b2da88443ed84bc02835217971
rentbest Asus <rinat.davleev.97@gmail.com>
2025-03-02 13:21:27 +0300

    ADD: - method for User Database

--- app/db/models/users.py
+++ app/db/models/users.py
@@ -1,9 +1,17 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import Column, String
-from fastapi_users.db import SQLAlchemyBaseOAuthAccountTable
+from fastapi_users.db import SQLAlchemyBaseOAuthAccountTable, SQLAlchemyUserDatabase
 
 from app.db.base import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseOAuthAccountTable[int]):
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
