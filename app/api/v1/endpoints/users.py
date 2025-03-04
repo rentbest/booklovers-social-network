a0d0d5f8738763805b8fc783282459e7753709f5
rentbest Asus <rinat.davleev.97@gmail.com>
2025-03-04 22:59:29 +0300

    ADD: setup for User's endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -6,9 +6,10 @@ from app.db.session import get_session
 from app.db.models.users import User
 
 
-router = APIRouter(tags=["Users"])
+router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.get("/users/")
+# Только для админов
+@router.get("/")
 async def read_users(
     db: AsyncSession = Depends(get_session),
 ):
@@ -16,3 +17,28 @@ async def read_users(
 
     result = await db.execute(query)
     return result.scalars().all()
+
+
+@router.post("/register/")
+def login():
+    pass
+
+
+@router.post("/login/")
+def login():
+    pass
+
+
+@router.get("/me/")
+def protected():
+    pass
+
+
+@router.get("/me/change-password")
+def protected():
+    pass
+
+
+@router.delete("/me/")
+def protected():
+    pass
