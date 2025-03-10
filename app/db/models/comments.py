a275e54bca804562c61a1ebb7a03babed22185ea
rentbest Asus <rinat.davleev.97@gmail.com>
2025-03-10 22:06:40 +0300

    modified comments structure

--- app/db/models/comments.py
+++ app/db/models/comments.py
@@ -1,8 +1,22 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
+from app.utils.utc_now import utc_now
 from app.db.base import Base
 
-class Comment(Base):
+
+class BaseComment(Base):
+    __abstract__ = True
+
     text = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    book_id = Column(Integer, ForeignKey("books.id"))
+    created_at = Column(DateTime, default=utc_now)
+    updated_at = Column(DateTime, default=utc_now, onupdate=utc_now)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+
+class BookComment(BaseComment):
+    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
+
+
+class ReviewComment(BaseComment):
+    review_id = Column(Integer, ForeignKey("reviews.id"), nullable=False)
