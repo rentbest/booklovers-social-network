7efa040f5e8df7865604c69f0d41df905185bef9
rentbest Asus <rinat.davleev.97@gmail.com>
2025-03-10 22:21:59 +0300

    ADD: fixed comments models..

--- app/db/models/comments.py
+++ app/db/models/comments.py
@@ -7,7 +7,6 @@ from app.db.base import Base
 class BaseComment(Base):
     __abstract__ = True
 
-    id = Column(Integer, primary_key=True, index=True)
     text = Column(String, nullable=False)
     created_at = Column(DateTime, default=utc_now)
     updated_at = Column(DateTime, default=utc_now, onupdate=utc_now)
@@ -16,8 +15,10 @@ class BaseComment(Base):
 
 
 class BookComment(BaseComment):
+    id = Column(Integer, primary_key=True, index=True)
     book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
 
 
 class ReviewComment(BaseComment):
+    id = Column(Integer, primary_key=True, index=True)
     review_id = Column(Integer, ForeignKey("reviews.id"), nullable=False)
